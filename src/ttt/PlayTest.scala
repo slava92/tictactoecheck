38953422670324405e3f449a0ce20e89f88c4c2e
slava92 <slava92@localhost>
2011-04-11 08:00:06 -0400

    update

--- src/ttt/PlayTest.scala
+++ src/ttt/PlayTest.scala
@@ -8,18 +8,24 @@ import tictactoe.RandomMoves
 object PlayTest {
 
   def main(args: Array[String]): Unit = {
-    //val game = new Game(RandomMoves, FirstAvailableMove) // p1: 494 wins, p2: 89 wins, draw: 417 times
-    //val game = new Game(FirstAvailableMove, RandomMoves) // p1: 255 wins, p2: 181 wins, draw: 564 times
-    val game = new Game(RandomMoves, RandomMoves) // p1: 371 wins, p2: 290 wins, draw: 339 times
-    val r1 = (0 until 1000).map {_ => game.play}.foldLeft((0,0,0)) { (sts, gr) => sts match {
-        case (p1,p2,dr) => gr.result match {
-            case Player1Wins => (p1+1,p2,dr)
-            case Player2Wins => (p1,p2+1,dr)
-            case Draw        => (p1,p2,dr+1)
-        }
+    val s1 = RandomMoves
+    val s2 = RandomMoves
+    val pits = (s1,s2) :: (s2,s1) :: Nil
+    val rs = pits.map { _ match {
+        case (p1, p2) =>
+          val game = new Game(RandomMoves, RandomMoves)
+          val r = (0 until 1000).map {_ => game.play}.foldLeft((0,0,0)) { (sts, gr) => sts match {
+              case (p1,p2,dr) => gr.result match {
+                  case Player1Wins => (p1+1,p2,  dr)
+                  case Player2Wins => (p1,  p2+1,dr)
+                  case Draw        => (p1,  p2,  dr+1)
+                }
+            }
+          }
+          r
       }
-    }
-    r1 match {
+    }.foldLeft((0,0,0)) { (z,c) => (z._1+c._1,z._2+c._2,z._3+c._3) }
+    rs match {
       case (p1,p2,dr) => printf("p1: %d wins, p2: %d wins, draw: %d times\n", p1, p2, dr)
     }
   }
