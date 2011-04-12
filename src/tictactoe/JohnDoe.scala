065f18f2b9ba9681c157bbbf4c4230a7fb361033
slava92 <slava92@localhost>
2011-04-12 18:04:16 -0400

    update

--- src/tictactoe/JohnDoe.scala
+++ src/tictactoe/JohnDoe.scala
@@ -4,33 +4,32 @@ import fj.F
 import fj.P
 
 object JohnDoe extends Strategy {
+
+  type Weight = (Int,Position)
+
   def firstMove(): Board = {
     Board.EmptyBoard.empty.moveTo(Position.C)
   }
-  private def neutral(p: Position): F[Board,(Int,Position)] = {
-    new F[Board,(Int,Position)]() {
+  private def neutral(p: Position): F[Board,Weight] = {
+    new F[Board,Weight]() {
       def f(b: Board) = (0,p)
     }
   }
-  private def gameOver(p: Position): F[Board.FinishedBoard,(Int,Position)] = {
-    new F[Board.FinishedBoard,(Int,Position)]() {
+  private def gameOver(p: Position): F[Board.FinishedBoard,Weight] = {
+    new F[Board.FinishedBoard,Weight]() {
       def f(b: Board.FinishedBoard) = (1,p)
     }
   }
 
+  private def weighPosition(b: Board, p: Position): Weight = {
+    b.moveTo(p).fold(P.p((Int.MinValue,p)),neutral(p),gameOver(p))
+  }
   def nextPosition(b: Board): Position = {
     if (b.playerAt(Position.C).isNone) {
       return Position.C
     }
-    val weights = Position.values.map {
-      p => b.moveTo(p).fold(P.p((Int.MinValue,p)),neutral(p),gameOver(p))
-    }.sortWith { _._1 < _._1
-    }.reverse
+    val weights = Position.values.map {weighPosition(b, _)
+    }.sortWith {_._1 < _._1}.reverse
     return weights(0)._2
-//    if (0 < weights(0)._1) {
-//      return weights(0)._2
-//    } else {
-//      return RandomMoves.nextPosition(b)
-//    }
   }
 }
