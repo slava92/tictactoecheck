633fc6f31f9897ecccc7c90f61a8105471dd19d9
slava92 <slava92@localhost>
2011-04-12 22:18:26 -0400

    update

--- src/tictactoe/JohnDoe.scala
+++ src/tictactoe/JohnDoe.scala
@@ -6,30 +6,39 @@ import fj.P
 object JohnDoe extends Strategy {
 
   type Weight = (Int,Position)
+  val BigBang = 100
 
   def firstMove(): Board = {
     Board.EmptyBoard.empty.moveTo(Position.C)
   }
-  private def neutral(p: Position): F[Board,Weight] = {
+  private def neutral(p: Position, s: Int): F[Board,Weight] = {
     new F[Board,Weight]() {
-      def f(b: Board) = (0,p)
+      def f(b: Board) = {
+        val weights = Position.values.map {weighPosition(b, _, (-s))
+        }.sortWith {_._1 < _._1}.reverse
+        //(0,p)
+        weights(0)
+      }
     }
   }
-  private def gameOver(p: Position): F[Board.FinishedBoard,Weight] = {
+  private def gameOver(p: Position, s: Int): F[Board.FinishedBoard,Weight] = {
     new F[Board.FinishedBoard,Weight]() {
-      def f(b: Board.FinishedBoard) = (1,p)
+      def f(b: Board.FinishedBoard) = ((BigBang-b.nmoves())*s,p)
     }
   }
 
-  private def weighPosition(b: Board, p: Position): Weight = {
-    b.moveTo(p).fold(P.p((Int.MinValue,p)),neutral(p),gameOver(p))
+  private def weighPosition(b: Board, p: Position, s: Int): Weight = {
+    b.moveTo(p).fold(P.p((Int.MinValue,p)),neutral(p,s),gameOver(p,s))
   }
   def nextPosition(b: Board): Position = {
     if (b.playerAt(Position.C).isNone) {
       return Position.C
     }
-    val weights = Position.values.map {weighPosition(b, _)
+    val weights = Position.values.map {weighPosition(b, _, 1)
     }.sortWith {_._1 < _._1}.reverse
-    return weights(0)._2
+//    weights.foreach { wp =>
+//      printf("%2s %s\n", wp._2.toString, wp._1.toString)
+//    }
+    weights(0)._2
   }
 }
--- src/ttt/PlayTest.scala
+++ src/ttt/PlayTest.scala
@@ -12,15 +12,20 @@ object PlayTest {
   def main(args: Array[String]): Unit = {
     val s1 = JohnDoe
     val s2 = RandomMoves
-    tourney(s1, s2)
+    tourney(s1, s2, 10)
+    //oneGame(s1,s2)
   }
 
-  def tourney(s1: Strategy, s2:Strategy) {
+  def oneGame(s1: Strategy, s2: Strategy) {
+    val result = new Game(s1,s2).play
+    printBoard(result)
+  }
+  def tourney(s1: Strategy, s2: Strategy, runs: Int) {
     val pits = (s1,s2) :: (s2,s1) :: Nil
-    val rs = pits.map { _ match {
+    val rss = pits.map { _ match {
         case (p1, p2) =>
           val game = new Game(p1, p2)
-          val r = (0 until 1000).map {_ => game.play}.foldLeft((0,0,0)) { (sts, gr) => sts match {
+          val r = (0 until runs).map {_ => game.play}.foldLeft((0,0,0)) { (sts, gr) => sts match {
               case (p1,p2,dr) => gr.result match {
                   case Player1Wins => (p1+1,p2,  dr)
                   case Player2Wins => (p1,  p2+1,dr)
@@ -30,7 +35,8 @@ object PlayTest {
           }
           r
       }
-    }.foldLeft((0,0,0)) { (z,c) => (z._1+c._1,z._2+c._2,z._3+c._3) }
+    } //.foldLeft((0,0,0)) { (z,c) => (z._1+c._1,z._2+c._2,z._3+c._3) }
+    val rs = (rss(0)._1+rss(1)._2,rss(0)._2+rss(1)._1,rss(0)._3+rss(1)._3)
     printf("p1: %d wins, p2: %d wins, draw: %d times\n", rs._1, rs._2, rs._3)
   }
   def printBoard(b: BoardLike) {
